c3022aeb67081d7405cb003c739af62ddc00e1a8
Chrapati <des.abeilles.et.du.miel.24@gmail.com>
2020-10-30 14:07:41 +0100

    first commit

---
+++ obj/test.d
@@ -0,0 +1,15 @@
+obj/test.o: src/main.c src/libs/Sokol/sokol_app.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/lib/gcc/i686-w64-mingw32/8.1.0/include/stdint.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/stdint.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/crtdefs.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/_mingw.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/_mingw_mac.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/_mingw_secapi.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/vadefs.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/sdks/_mingw_directx.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/sdks/_mingw_ddk.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/lib/gcc/i686-w64-mingw32/8.1.0/include/stddef.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/stddef.h \
+ E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/lib/gcc/i686-w64-mingw32/8.1.0/include/stdbool.h \
+ src/libs/Sokol/sokol_gfx.h src/libs/Sokol/sokol_glue.h
+Cwc:VLianceTool/LibRT Default gcc (12/05/2018 11:31:37) >>  -std=gnu99 -ggdb -I"E:/Travail/chrapatia/projets/gameEngine/" -m32 -I"E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/include/" -I"E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/" -I"E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/c++/" -I"E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/i686-w64-mingw32/include/c++/backward/" -I"E:/program_files/CWC/Toolchain/VLianceTool/LibRT/LibRT-8.0.6/pthreads/" -MD -DDsToolchain="VLianceTool_LibRT" -DDsPlatform="Windows" -DD_Platform_Windows -DDsPlatform_File="Default" -DD_Debug -DDsOpt="Debug" -c src/main.c -o obj/test.o
